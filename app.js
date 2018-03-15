251cb5d7d1fb37350473f84d6520727239577fb4
MariaGiurgiu <marya_giurgiu@yahoo.com>
2018-03-15 11:47:52 +0200

    Remove comments

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 let express = require('express');
-let app = express();
-
 let fs = require('fs');
 let request = require('request');
+let app = express();
 
 app.set('port', 3000);
 
 app.get('/', function (req, res) {
-    // request({
-    //     uri: 'http://labs.bible.org/api/?passage=random'
-    // }).pipe(res)
     request('http://labs.bible.org/api/?passage=random', function (err, response, body) {
         fs.readFile('./index.html', 'utf8', function(err, data){
             if(err) throw err;
@@ -18,8 +14,6 @@ app.get('/', function (req, res) {
             res.send(document)
         });
     });
-
-
 });
 
 app.listen(3000);
