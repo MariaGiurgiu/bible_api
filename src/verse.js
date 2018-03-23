d1c17797d1b82f2ef301e80ccc7417ce7590b994
Corneliu <vesacorneliu@yahoo.com>
2018-03-24 00:18:14 +0200

    Add verse's datetimes related TODOs

--- src/verse.js
+++ src/verse.js
@@ -2,7 +2,7 @@ class Verse {
     constructor(text, datetime) {
         this.id = Math.floor((Math.random() * 100) + 1); // TODO Remove collision risk! See https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
         this.text = text;
-        this.datetime = datetime;
+        this.datetime = datetime; // TODO Generate 'now' date and time. Use something like: new Date(Date.now()).toLocaleString(); or a library like http://momentjs.com/
     }
 
     getText() {
