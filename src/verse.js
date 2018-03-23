eea8225b3fba2797a4711be1fa3eebfe446e465a
Corneliu <vesacorneliu@yahoo.com>
2018-03-24 00:09:47 +0200

    TODO Remove uid collision risk

--- src/verse.js
+++ src/verse.js
@@ -1,6 +1,6 @@
 class Verse {
     constructor(text, datetime) {
-        this.id = Math.floor((Math.random() * 100) + 1);
+        this.id = Math.floor((Math.random() * 100) + 1); // TODO Remove collision risk! See https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
         this.text = text;
         this.datetime = datetime;
     }
