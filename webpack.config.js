5dda7d8fe192dcacdc973651531ccd5ae64031fd
Corneliu <vesacorneliu@yahoo.com>
2018-03-27 09:49:42 +0300

    Improve Webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,8 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        plugins: ['../node_modules/babel-plugin-transform-react-jsx']
-                    }
-                }
-            },
-            {
                 exclude: /node_modules/, // don't transpile node_modules
-                test: /\.jsx$/,          // do transpile any files ending in .jsx
+                test: /\.js[x]?$/,          // do transpile any files ending in .jsx
                 use: {
                     loader: 'babel-loader',
                     options: {
