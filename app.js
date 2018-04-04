e4c34caff42709c3ce679a9cf82afca260b3b3ed
MariaGiurgiu <marya_giurgiu@yahoo.com>
2018-04-04 16:09:49 +0300

    Prepare for redis

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 let express = require('express');
-let striptags = require('striptags');
 let fs = require('fs');
 let axios = require('axios');
 let app = express();
+let redis = require('redis');
+
+let client = redis.createClient();
+client.on('error', function(err){
+    console.log('Something went wrong ', err)
+});
+
+client.set('my test key', 'my test value', redis.print);
+client.get('my test key', function(error, result) {
+    if (error) throw error;
+    console.log('GET result ->', result)
+});
 
 app.use(express.static('static'));
 app.use(express.static('dist'));
+
+
+app.get('/get', function (req, res) {
+    
+});
+
 
 app.get('/data', function (req, res) {
     axios.get('http://labs.bible.org/api/?passage=random&formatting=plain&type=json')
         .then(function(resp) {
             let result = resp.data[0];
-console.log(result)
             res.send(result)
         }).catch(function(err) {
             res.send(err)
@@ -26,6 +42,7 @@ app.get('/', function (req, res){
     });
 
 });
+
 
 app.listen(3000);
 
---
+++ src/verseRepositoryBackend.js
@@ -0,0 +1,23 @@
+export default class VerseRepositoryBackend {
+    findAll(){
+
+    }
+
+    getOneById(id){
+
+    }
+
+    add(v){
+
+    }
+
+    like(id){
+
+    }
+
+    remove(id){
+
+    }
+
+
+}
